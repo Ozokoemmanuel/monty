2cbb129a41e02a0f56f16e6b852e174359b2db22
Ozokoemmanuel <pcozoko@gmail.com>
2022-10-07 05:30:07 +0100

    monty

--- 5-handlers.c
+++ 5-handlers.c
@@ -1,99 +1,102 @@
 #include "monty.h"
 
-
 /**
- * pop_handler - a function that removes the top element of the stack
+ * pchar_handler -  prints the char at the top of the stack,
+ * followed by a new line
  * @stack: double pointer to the stack
- * @line_number: the line number where the pop command is called
- *
+ * @line_no: the line in which this command is called
  * Return: nothing
  */
-void pop_handler(stack_t **stack, unsigned int line_number)
+void pchar_handler(stack_t **stack, unsigned int line_no)
 {
-	stack_t *temp;
-
 	if (!(*stack)->next)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_no);
 		global.err_status = EXIT_FAILURE;
 		return;
 	}
-	temp = (*stack)->next->next;
-	free((*stack)->next);
-	if (temp)
-		temp->prev = *stack;
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_no);
+		global.err_status = EXIT_FAILURE;
+		return;
+	}
 
-	(*stack)->next = temp;
+	printf("%c\n", (*stack)->next->n);
 }
 
-/**
- * swap_handler - swaps the top two elements of the stack
- * @stack: double pointer to the stack
- * @line_no: the line number where the pop command is called
- * Return: nothing
- */
-void swap_handler(stack_t **stack, unsigned int line_no)
-{
-	stack_t *temp = (*stack)->next;
-	int stack_len;
 
-	stack_len = stack_size((*stack)->next);
-	if (stack_len < 2)
+/**
+ * pstr_handler - Prints the string contained in a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void pstr_handler(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_no);
-		global.err_status = EXIT_FAILURE;
-		return;
+		printf("%c", tmp->n);
+		tmp = tmp->next;
 	}
-	temp = temp->next;
-	(*stack)->next->next = temp->next;
-	(*stack)->next->prev = temp;
-	if (temp->next)
-		temp->next->prev = (*stack)->next;
-	temp->next = (*stack)->next;
-	temp->prev = *stack;
-	(*stack)->next = temp;
+
+	printf("\n");
+
+	(void)line_number;
 }
 
+
+
 /**
- * add_handler - adds the top two elements of the stack
- * @stack: double pointer to the stack
- * @line_no: the line number where the pop command is called
- * Return: nothing
+ * rotr_handler - Rotates the bottom value of a stack_t linked list to the top.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
-void add_handler(stack_t **stack, unsigned int line_no)
+void rotr_handler(stack_t **stack, unsigned int line_number)
 {
-	int size = stack_size((*stack)->next);
+	stack_t *top, *bottom;
 
-	if (size < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_no);
-		global.err_status = EXIT_FAILURE;
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 		return;
-	}
-	(*stack)->next->next->n += (*stack)->next->n;
 
-	pop_handler(stack, line_no);
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
+	(void)line_number;
 }
 
+
 /**
- * sub_handler - subtracts the top element of the stack
- *				from the second top element
- * @stack: double pointer to the stack
- * @line_no: the line number where the pop command is called
- * Return: nothing
+ * rotl_handler - Rotates the top value of a stack_t linked list to the bottom.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
-void sub_handler(stack_t **stack, unsigned int line_no)
+void rotl_handler(stack_t **stack, unsigned int line_number)
 {
-	int size = stack_size((*stack)->next);
+	stack_t *top, *bottom;
 
-	if (size < 2)
-	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_no);
-		global.err_status = EXIT_FAILURE;
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 		return;
-	}
 
-	(*stack)->next->next->n -= (*stack)->next->n;
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
 
-	pop_handler(stack, line_no);
+	top->next->prev = *stack;
+	(*stack)->next = top->next;
+	bottom->next = top;
+	top->next = NULL;
+	top->prev = bottom;
+
+	(void)line_number;
 }
